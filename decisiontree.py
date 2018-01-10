3ce944eea74a28d41f75ea1edf7f4234b66d6315
Jacques Caspi <jrcaspi@gmail.com>
2018-01-10 10:47:00 -0500

    Update decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -1,9 +1,16 @@
+#Improting the Decision Tree package
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import tree
+
+#We define the criteria of the decision tree and fit the model
 clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100, max_depth=3, min_samples_leaf=5)
 clf_gini.fit(X_train, y_train)
+
+#We predict the outcome variable using standardized X variables
 y_pred=clf_gini.predict(X_test_std)
+
+#We print Misclassification, Accuracy, Confusion Matrix, Classification Report and RMSE
 print ('Misclassified for Tree samples: %d' % (y_test != y_pred).sum())
 from sklearn.metrics import classification_report, confusion_matrix
 print ('Tree Accuracy: %.2f' % (accuracy_score(y_test, y_pred)))
@@ -17,6 +24,7 @@ rms = sqrt(mean_squared_error(y_test, y_pred))
 print('RMSE: ')
 print(rms)
 
+#We print the ROC curve
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
